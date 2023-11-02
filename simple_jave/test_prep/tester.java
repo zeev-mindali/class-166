7cb37c2dd356a9ece2229cd9bb929586d3eac6b4
zeev mindali <mindalizeev@gmail.com>
2023-11-02 20:01:11 +0200

    Update tester.java

--- simple_jave/test_prep/tester.java
+++ simple_jave/test_prep/tester.java
@@ -1,1 +1,9 @@
+package simple_jave.test_prep;
+
+public class myTester {
+   public static void main(String[] args) {
+//        Random r = new Random();
+//        System.out.println(Department.depNames[r.nextInt(Department.depNames.length)]);
+   }
+}
 
