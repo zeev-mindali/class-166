5bea8f11cbd128179a720b05a0578b63794a1f9a
zeev mindali <mindalizeev@gmail.com>
2023-11-02 20:18:57 +0200

    prep exam

--- simple_jave/test_prep/test/testCompany.java
+++ simple_jave/test_prep/test/testCompany.java
@@ -1,12 +1,28 @@
 package simple_jave.test_prep.test;
 
 import simple_jave.test_prep.cls.Company;
+import simple_jave.test_prep.cls.Programmer;
+import simple_jave.test_prep.cls.Salary;
+
+import java.util.Random;
 
 public class testCompany {
     public static Company createCompany(){
         //process
+        Random rnd = new Random();
+        String[] names = {"Gideon","Alexey","Yonatan","Denis","Doron"};
+        //create programmers
+        Programmer[] programmers = new Programmer[30];
+        for (int index=0;index<30;index++){
+            programmers[index] = new Programmer(
+                    names[rnd.nextInt(names.length)],
+                    rnd.nextInt(10),
+                    new Salary(rnd.nextDouble()*100_000,rnd.nextInt(100_000))
+            );
+        }
 
         //return our new and shiny company
+
         return null;
     }
 }
