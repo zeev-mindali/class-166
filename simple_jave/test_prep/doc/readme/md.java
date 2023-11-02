c502083b2dd0c4e5d3c07681f1851afb17b6ba91
zeev mindali <mindalizeev@gmail.com>
2023-11-02 21:13:49 +0200

    prep exam

---
+++ simple_jave/test_prep/doc/readme/md.java
@@ -0,0 +1,69 @@
+package simple_jave.test_prep.doc.readme;
+
+public class md {
+    /*
+
+    מחלקות הבאות:
+•	salary
+o	amount: double (10_000, 80_000)
+o	bonus:int 0-100_000
+•	worker
+o	name: string
+o	expirance : int
+•	programmer -> worker
+o	salary: salary
+•	team_leader->worker
+o	condition: salary above 20_000
+•	manager->worker
+o	condition: salary above 35_000
+o	position:string -> ceo, coo, cmo, cto, cio, and raz
+•	department
+o	name:string -> programming, qa, marketing, administration
+o	manager: manager
+o	employee up to 15 worker
+•	company
+o	deperments up to  3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+כתבו מחלקה בשם testCompany אשר מייצרת את החברה כולה (איתחול כל המחלקות) בצורה סטטית.
+את השמות ניתן לייצר כמערך אשר נגריל את המיקום שלו.
+
+
+
+הטסטר ידפיס את הנתונים הבאים:
+1.	משכורת ממוצעת
+2.	השם שהנפוץ ביותר.
+3.	הבונוס הנמוך ביותר.
+4.	הבונוס הגבוה ביותר.
+5.	המשכורת הגבוה ביותר
+6.	המשכורת הנמוכה ביותר
+7.	רשימת עובדים שלא קיבלו בונוס.
+
+
+
+     */
+}
