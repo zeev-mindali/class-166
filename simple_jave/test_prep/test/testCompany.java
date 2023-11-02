dec9a7ec17427b7bc9d3c7a866db63c7632e0c53
zeev mindali <mindalizeev@gmail.com>
2023-11-02 19:55:49 +0200

    prep exam

---
+++ simple_jave/test_prep/test/testCompany.java
@@ -0,0 +1,12 @@
+package simple_jave.test_prep.test;
+
+import simple_jave.test_prep.cls.Company;
+
+public class testCompany {
+    public static Company createCompany(){
+        //process
+
+        //return our new and shiny company
+        return null;
+    }
+}
