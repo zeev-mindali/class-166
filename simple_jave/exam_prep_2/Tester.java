6e7777383c6258d26d262a579fce6175123680fb
zeev mindali <mindalizeev@gmail.com>
2023-11-06 19:22:11 +0200

    prep exam

--- simple_jave/exam_prep_2/Tester.java
+++ simple_jave/exam_prep_2/Tester.java
@@ -1,9 +1,11 @@
 package simple_jave.exam_prep_2;
 
 import simple_jave.exam_prep_2.cls.Company;
+import simple_jave.exam_prep_2.my_test.CompanyTester;
 
 public class Tester {
     public static void main(String[] args) {
-
+        Company company = CompanyTester.createCompany();
+        System.out.println(company);
     }
 }
---
+++ simple_jave/exam_prep_2/my_test/CompanyTester.java
@@ -0,0 +1,10 @@
+package simple_jave.exam_prep_2.my_test;
+
+import simple_jave.exam_prep_2.cls.Company;
+
+public class CompanyTester {
+    public static Company createCompany(){
+        //create two migrash, two car, one sold
+        return new Company()
+    }
+}
